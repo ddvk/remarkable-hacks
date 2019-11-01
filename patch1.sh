4bc678a4bd32567277ad228106e9c4f3868945b6
ddvk <ddvk>
2019-11-01 16:59:43 +0100

    use device bspatch

--- patch1.sh
+++ patch1.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-wget https://github.com/ddvk/remarkable-hacks/raw/master/tools/bspatch
 wget https://github.com/ddvk/remarkable-hacks/raw/master/patches/patch1
 cp /usr/bin/xochitl .
-chmod +x bspatch
-./bspatch xochitl xochitl.patched patch1
+bspatch xochitl xochitl.patched patch1
 chmod +x xochitl.patched

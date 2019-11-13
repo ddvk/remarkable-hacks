22c717695532665937d465b59af1b78048c7e252
ddvk <ddvk>
2019-11-13 13:50:56 +0100

    add liblcms binary

--- tools/screenshot.sh
+++ tools/screenshot.sh
@@ -1,2 +1,2 @@
 ssh rm "cat /dev/fb0" |
-  ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt gray16le -s 1408,1872 -i - -vframes 1 -f image2 -vcodec png $1
+  ffmpeg -vcodec rawvideo -f rawvideo -pix_fmt gray16le -s 1408,1872 -i - -vframes 1 -vf scale=600x800 -f image2 -vcodec png $1

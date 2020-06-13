27451dcd57ab122b18e776d4a1954174186317b6
ddvk <ddvk>
2020-06-13 21:43:00 +0200

    patch_07 default, take care of #10

--- patch.sh
+++ patch.sh
@@ -1,5 +1,14 @@
 set -e
 binary_name=xochitl
+workdir=/home/rmhacks
+patched="$workdir/$binary_name.patched"
+
+if [ ! -d $workdir ]; then
+    mkdir -p $workdir
+    # migrate stuff and clean old patches
+    mv /home/root/xochitl.* $workdir 2> /dev/null || true
+    rm /home/root/patch_* 2> /dev/null || true
+fi;
 
 function checkspace(){
     available=$(df / | tail -n1 | awk '{print $4}');
@@ -25,7 +34,7 @@ function onexit(){
 function cleanup(){
     echo "Cleaning up..."
     rm /tmp/*crash* 2> /dev/null || true
-    rm -fr .cache/remarkable/xochitl/qmlcache/*
+    rm -fr /home/root/.cache/remarkable/xochitl/qmlcache/*
 }
 
 function rollback(){
@@ -41,14 +50,14 @@ function auto_install(){
     read yn
     case $yn in 
         [Yy]* ) 
-            echo "Making it permanent, DON'T DELETE $backup_file"
-            mv $binary_name.patched /usr/bin/$binary_name
+            echo "Making it permanent, DON'T DELETE $backup_file !!!"
+            mv $patched /usr/bin/$binary_name
             echo "Starting the UI..."
             systemctl start xochitl
             return 0
             ;;
         * )
-            echo "Use the xochitl.patched binary if you change your mind / provide it if it segfaulted."
+            echo "Use the $patched binary if you change your mind / provide it if it segfaulted."
             echo "Starting the original..."
             systemctl start xochitl
             ;;
@@ -58,7 +67,7 @@ function auto_install(){
 
 case $(</etc/version) in
     "20200320131825" )
-        patch_name=${1:-patch_06}
+        patch_name=${1:-patch_07}
         version="2113"
         expectedhash="c8661fbd74a049134509dc22da415bb651d7feac"
         echo "Version 2.1.1.3 - $patch_name"
@@ -83,7 +92,7 @@ case $(</etc/version) in
 esac
 
 
-backup_file="${binary_name}.${version}"
+backup_file="$workdir/$binary_name.$version"
 
 if [ $patch_name == "rollback" ]; then
     rollback
@@ -91,7 +100,7 @@ if [ $patch_name == "rollback" ]; then
 fi
 
 if [ -z "$SKIP_DOWNLOAD" ]; then
-    wget "https://github.com/ddvk/remarkable-hacks/raw/master/patches/$version/$patch_name" -O $patch_name || exit 1
+    wget "https://github.com/ddvk/remarkable-hacks/raw/master/patches/$version/$patch_name" -O "$workdir/$patch_name" || exit 1
 fi
 
 #make sure we keep the original, which is needed for additional patching or rollback
@@ -106,10 +115,8 @@ if [ "$expectedhash" != "$hash" ]; then
 fi
 
 
-cp $backup_file $binary_name
-
-bspatch $binary_name $binary_name.patched $patch_name
-chmod +x xochitl.patched
+bspatch $backup_file $patched $workdir/$patch_name
+chmod +x $patched
 
 systemctl stop xochitl
 #just to be sure, it goes into and endless reboot due to qml mismatch
@@ -118,5 +125,5 @@ systemctl stop remarkable-fail
 cleanup
 echo "Trying to start the patched version..."
 echo "You can play around, press CTRL-C when done."
-QT_LOGGING_RULES=*=false ./xochitl.patched -plugin evdevlamy  || echo "It crashed?"
+QT_LOGGING_RULES=*=false $patched -plugin evdevlamy  || echo "Did it crash?"
 cleanup

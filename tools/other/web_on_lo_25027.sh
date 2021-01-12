750303ce847183bae34235f702c277dbfef758a0
ddvk <ddvk>
2021-01-12 16:41:41 +0100

    add usb0 patching script

---
+++ tools/other/web_on_lo_25027.sh
@@ -0,0 +1,5 @@
+# changes the web server listen interface to lo (to make it accessible over the network via ssh)
+$file=$1
+./binpatch.py $file 0813fc "0210a0e3"
+./binpatch.py $file 08149c "a30000ea"
+./binpatch.py $file 3a3538 "6c6f0000"
